898c600d1533039f94e6ec5b377ac8317f288f53
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-19 19:14:21 +0300

    added validations to pizza

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,5 +1,6 @@
 class Pizza < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :restaurants, through: :restaurant_pizzas
-   
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
