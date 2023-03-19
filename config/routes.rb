f9b9dff6f2dd6e85ef7de9fef9bb0976d41876e9
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-19 19:15:11 +0300

    removed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurant_pizzas
-  resources :pizzas
-  resources :restaurants
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
