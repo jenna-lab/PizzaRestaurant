ab98a78f06dfc1d1d292c3816e51b9479b62451a
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-19 19:13:17 +0300

    added validation to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
     has_many :pizzas, through: :restaurant_pizzas
     has_many :restaurant_pizzas
-    
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
